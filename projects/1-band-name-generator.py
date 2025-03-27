c0fac54e5e1f6ff083e0a1b52b33313fc0629440
Lucas Keller <lucaskeller27@gmail.com>
2025-03-26 23:24:32 -0300

    Day 2 completed

---
+++ projects/1-band-name-generator.py
@@ -0,0 +1,9 @@
+# Day 1 Project – Band name generator
+
+print("### Band Name Generator ###")
+
+city = input("What is the name of the city you're from?\n-> ")
+pet = input("If you had just adopted a pet animal, what would you name it?\n-> ")
+
+print("Your band name is: #", city, pet, "#")
+# print(f"Your band name is: # {city} {pet} #")
---
+++ projects/2-tip-calculator.py
@@ -0,0 +1,15 @@
+# Day 2 Project – Tip calculator
+
+print("### Tip Calculator ###")
+
+bill = float(input("What was the total bill?\n-> $"))
+tip = int(input("What percentage tip would you like to give? (Answer with numbers only)\n-> "))
+billSplitters = int(input("And how many people are splitting the bill?\n-> "))
+
+# total = (bill * ((tip / 100) + 1))
+total = (((tip / 100) * bill) + bill)
+# print(total)
+share = round((total / billSplitters), 2)
+
+# print(f"Each person should pay ${share}")
+print("Each person should pay $%.2f" %(share))
