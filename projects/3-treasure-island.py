e00b87212b86d8b0f6fd945adf74462463336861
Lucas Keller <lucaskeller27@gmail.com>
2025-04-01 18:22:49 -0300

    Minor changes

--- projects/3-treasure-island.py
+++ projects/3-treasure-island.py
@@ -1,5 +1,6 @@
 
 # Day 3 Project - Treasure Island
+
 print(r'''
 *******************************************************************************
           |                   |                  |                     |
@@ -25,21 +26,21 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.\n")
 print("Your mission is to find the treasure.")
 
-first_choice = input("You see two separate paths in front of you. Do you want to go left or right? (Type L or R)\n-> ")
+choice_one = input("You see two separate paths in front of you. Do you want to go left or right? (Type L or R)\n-> ")
 
-if first_choice == "R":
+if choice_one == "R":
     print("You run into a snake, get bitten and sadly pass away. Game over.")
-elif first_choice == "L":
-    second_choice = input("You arrive at a lake with an island in the middle. Do you want to walk around it or try to swim across? (Type W or S)\n-> ")
-    if second_choice == "S":
+elif choice_one == "L":
+    choice_two = input("You arrive at a lake with an island in the middle. Do you want to walk around it or try to swim across? (Type W or S)\n-> ")
+    if choice_two == "S":
         print("You get attacked by a shark and tragically pass away. Game over.")
-    elif second_choice == "W":
-        third_choice = input("You find some logs and make a raft to cross the lake. You arrive at the island and find a house with 3 doors: one red, one yellow, and one blue. Which one do you want to open? (Type R, Y or B)\n-> ")
-        if third_choice == "R":
+    elif choice_two == "W":
+        choice_three = input("You find some logs and make a raft to cross the lake. You arrive at the island and find a house with 3 doors: one red, one yellow, and one blue. Which one do you want to open? (Type R, Y or B)\n-> ")
+        if choice_three == "R":
             print("As you enter the room, you fall into a pit and sadly pass away. Game over.")
-        elif third_choice == "B":
+        elif choice_three == "B":
             print("You enter a room full of booby traps and get shot with a hundred arrows. Game over.")
-        elif third_choice == "Y":
+        elif choice_three == "Y":
             print("Congratulations, you found the treasure! You win!")
         else:
             print("Please try again and enter a valid option (R, Y or B).")
